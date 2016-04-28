28448993384e467f859b2fa6c648676ef353f8fa
Michael Adam <obnox@samba.org>
2016-04-28 11:27:37 +0200

    download.sh: consisten lazy evaluation in checks
    Use && rather than -a so second expression is not evaluated
    if first does not match.
    
    Signed-off-by: Michael Adam <obnox@samba.org>

--- common/download.sh
+++ common/download.sh
@@ -34,12 +34,12 @@ elif [ $RELEASE = 'squeeze' ] || [ $RELEASE = 'wheezy' ]; then
   utils.lxc.create -t debian -- \
                    --release ${RELEASE} \
                    --arch ${ARCH}
-elif [ ${DISTRIBUTION} = 'fedora' -a "${RELEASE}" = 'rawhide' ]; then
+elif [ ${DISTRIBUTION} = 'fedora' ] && [ "${RELEASE}" = 'rawhide' ]; then
   ARCH=$(echo ${ARCH} | sed -e "s/38/68/" | sed -e "s/amd64/x86_64/")
   utils.lxc.create -t fedora --\
                    --release ${RELEASE} \
                    --arch ${ARCH}
-elif [ ${DISTRIBUTION} = 'fedora' -a ${RELEASE} -ge 21 ]; then
+elif [ ${DISTRIBUTION} = 'fedora' ] && [ ${RELEASE} -ge 21 ]; then
   ARCH=$(echo ${ARCH} | sed -e "s/38/68/" | sed -e "s/amd64/x86_64/")
   utils.lxc.create -t fedora --\
                    --release ${RELEASE} \
